085a4bdcdec673606956715e0b92ea3d0d79f253
lynda-trad <lynda.trad@laplateforme.io>
2021-09-28 11:55:45 +0200

    Add job 05, 11, 13, 17

---
+++ jour01/job11/main.py
@@ -0,0 +1,9 @@
+def add(x,y):
+    return x+y
+
+# Test
+while True:
+    num1 = int(input("Enter the first number\n"))
+    num2 = int(input("Enter the second number\n"))
+    print(add(num1, num2))
+
---
+++ jour01/job17/main.py
@@ -0,0 +1,11 @@
+# List generator
+len = 5
+while len != 0:
+    print(len, " number(s) left.\n")
+    list.append(int(input("Please enter a number.")))
+    --len
+
+len = 4
+
+# Sorting
+def sorting(list):
