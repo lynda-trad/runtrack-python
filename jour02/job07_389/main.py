d5ea997ead3f260eaf99e7574148398075012880
lynda-trad <lynda.trad@laplateforme.io>
2021-09-29 14:48:53 +0200

    job07_389: Louer livre OK

--- jour02/job07_389/main.py
+++ jour02/job07_389/main.py
@@ -16,7 +16,7 @@ class Personne:
         self.nom = nom
 
     def sepresenter(self):
-        print("Auteur : ", self.prenom, self.nom)
+        print("Auteur : ", self.prenom, self.nom, "\n")
 
 
 class Auteur(Personne):
@@ -60,13 +60,13 @@ class Client(Personne):
 
     # affiche livres du client
     def inventaire(self):
-        print("Le client", self.prenom, self.nom, "a dans sa collection", self.collection)
+        print("Le client", self.prenom, self.nom, "a dans sa collection", self.collection, "\n")
 
-    def ajoutLivre(self, nom):
-        if nom in self.collection:
-            self.collection[nom] += 1
+    def ajoutLivre(self, livre):
+        if livre in self.collection:
+            self.collection[livre] += 1
         else:
-            self.collection[nom] = 1
+            self.collection[livre] = 1
 
 
 class Bibliotheque:
@@ -75,29 +75,28 @@ class Bibliotheque:
         self.collection = collection
 
     def acheterLivre(self, auteur, nom, quantite):
-        if nom in auteur.oeuvre:
+        if auteur.estDansLoeuvre(nom):
             if nom in self.collection:
                 self.collection[nom] += quantite
             else:
                 self.collection[nom] = quantite
 
     def inventaire(self):
-        print("La bibliotheque répertorie les livres :")
+        print("La bibliotheque répertorie :")
         for livre in self.collection:
-            print(self.collection[livre],"exemplaires de :")
+            print("-", self.collection[livre], "exemplaires de :")
             livre.printL()
+
+        print("\n")
 
 
     def louer(self, client, nom):
         # Look for the book
-        if nom in self.collection:
-            if self.collection[nom] != 0:
-                self.collection[nom] -= 1
-                client.ajoutLivre(nom)
-            else:
-                print("Sorry we do not have any", nom, "books left")
-        else:
-            print("We do not have", nom, "in our library !")
+        for livre in self.collection:
+            if livre.getTitre() == nom:
+                if self.collection[livre] != 0:
+                    self.collection[livre] -= 1
+                    client.ajoutLivre(nom)
 
     def rendreLivres(self, client):
         for livre in client.collection:
@@ -108,15 +107,16 @@ class Bibliotheque:
 
 
 # Creation d'un auteur
+print("---Creation d'un auteur---")
 A1 = Auteur("Jeff", "Vandermeer")
 A1.sepresenter()
 # Ajout des livres a l'oeuvre de l'auteur
 A1.ecrireUnLivre("Annihilation")
 A1.ecrireUnLivre("Autorité")
 A1.ecrireUnLivre("Acceptation")
-A1.listerOeuvre()
 
 # Creation de bibliotheque
+print("---Creation d'une bibliotheque---")
 collection = {
     A1.oeuvre[0]: 2,    #Annihilation
     A1.oeuvre[1]: 4,    #Autorité
@@ -124,16 +124,22 @@ collection = {
 bibliotheque = Bibliotheque("Joliette", collection)
 bibliotheque.inventaire()
 
-# Ajout du livre a la bibliotheque
-bibliotheque.acheterLivre(A1, "Acceptation", 3)
-print("Ajout du livre Acceptation en 3 exemplaires à la bibliothèque")
-bibliotheque.inventaire()
-
 # Creation de client
-
-A2 = Auteur("JRR", "Tolkien")
-A2.ecrireUnLivre("Lord of The Rings")
-print("\n")
+print("---Creation d'un client---")
 C1 = Client("Gilles", "Valentine")
 C1.ajoutLivre("Lord of the Rings")
 C1.inventaire()
+
+
+# Test louer
+print("---Le client loue Annihilation---")
+bibliotheque.louer(C1, "Annihilation")
+C1.inventaire()
+print("---La bibliotheque est mise à jour---")
+bibliotheque.inventaire()
+
+# Ajout d'un livre a la bibliotheque
+print("---Ajout du livre Acceptation en 3 exemplaires à la bibliothèque---\n")
+bibliotheque.acheterLivre(A1, "Acceptation", 3)
+bibliotheque.inventaire()
+
