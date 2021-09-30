52854776f10b53b30c9e2cc62058b1403a9ba596
lynda-trad <lynda.trad@laplateforme.io>
2021-09-30 11:28:44 +0200

    job02: add special characters

--- jour03/job02/main.py
+++ jour03/job02/main.py
@@ -2,14 +2,18 @@ from os.path import exists
 import re
 
 filename = "data.txt"
+count = 0
+special = ['.', ',', ';', '!', '\'', '\n', '?', '<', '>', '-', '_', '\\', '\"', ')', '(', 'é', '~', '`', '{', '}', '&', '=', '+', '%', '$', '£', '¨', '*', '²', ' ', '|', '#', '°', 'µ', '@']
 
 if exists(filename):
     file = open(filename)
     text = file.read()
-    count = text.split()
-    for word in count:
-        if word == ''
-    print(len(count))
+    words = text.split()
+    for word in words:
+        if word not in special: # caracteres speciaux
+            count += 1
+
+    print(count)
     file.close()
 else:
     print("File does not exist.")
