3a3e70c9422aab0fd6945b660fc3928733a4e07b
lynda-trad <lynda.trad@laplateforme.io>
2021-09-30 10:18:15 +0200

    job0_1 : print file

--- jour03/job0_1/main.py
+++ jour03/job0_1/main.py
@@ -1,10 +1,11 @@
 from os.path import exists
 if not exists("output.txt"):
-    file = open("output.txt", "x")
+    file = open("output.txt", "a+")
     file.write(input("Entrez une chaine de caractères "))
-    test = file.read()
-    print(test)
     file.close()
+
 else:
     print("File already exists.")
-    
+
+file = open("output.txt")
+print(file.read())
