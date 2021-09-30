7b0cfb0f1a5f0c98bde5b23883a75a256509e99b
lynda-trad <lynda.trad@laplateforme.io>
2021-09-30 17:05:13 +0200

    job13 : percentage

--- jour03/job13/main.py
+++ jour03/job13/main.py
@@ -3,82 +3,50 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 filename = 'data.txt'
-lowercase = {
-    'a': 0,
-    'b': 0,
-    'c': 0,
-    'd': 0,
-    'e': 0,
-    'f': 0,
-    'g': 0,
-    'h': 0,
-    'i': 0,
-    'j': 0,
-    'k': 0,
-    'l': 0,
-    'm': 0,
-    'n': 0,
-    'o': 0,
-    'p': 0,
-    'q': 0,
-    'r': 0,
-    's': 0,
-    't': 0,
-    'u': 0,
-    'v': 0,
-    'w': 0,
-    'x': 0,
-    'y': 0,
-    'z': 0
+alphabet = {
+    'a',
+    'b',
+    'c',
+    'd',
+    'e',
+    'f',
+    'g',
+    'h',
+    'i',
+    'j',
+    'k',
+    'l',
+    'm',
+    'n',
+    'o',
+    'p',
+    'q',
+    'r',
+    's',
+    't',
+    'u',
+    'v',
+    'w',
+    'x',
+    'y',
+    'z'
 }
-uppercase = {
-    'A': 0,
-    'B': 0,
-    'C': 0,
-    'D': 0,
-    'E': 0,
-    'F': 0,
-    'G': 0,
-    'H': 0,
-    'I': 0,
-    'J': 0,
-    'K': 0,
-    'L': 0,
-    'M': 0,
-    'N': 0,
-    'O': 0,
-    'P': 0,
-    'Q': 0,
-    'R': 0,
-    'S': 0,
-    'T': 0,
-    'U': 0,
-    'V': 0,
-    'W': 0,
-    'X': 0,
-    'Y': 0,
-    'Z': 0,
-}
-
-letters = []
+letters = {}
 
 if exists(filename):
     file = open(filename)
     text = file.read()
     file.close()
+    text.lower()
 
     words = text.split()
     for word in words:
-        for i in lowercase:
-            if word[0] == i:
-                lowercase[i] += 1
-
-        for i in uppercase:
-            if word[0] == i:
-                uppercase[i] += 1
-
-    for i in range(25):
-        letters.append(float(lowercase[i]) + float(uppercase[i]))
+        for al in alphabet:
+            if word[0] == al:
+                if al in letters:
+                    letters[al] += 1
+                else:
+                    letters[al] = 1
 
 else:
     print("File does not exist.")
@@ -101,6 +69,6 @@ fig, ax = plt.subplots()
 count = list(letters.keys())
 letter = list(letters.values())
 
-plt.bar(range(len(letters)), letter, tick_label=count)
+plt.bar(letters.keys(), letter, tick_label=count)
 plt.grid()
 plt.show()
