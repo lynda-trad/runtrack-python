65d243a02a8cff77aac678a42b6067cc42428bf1
lynda-trad <lynda.trad@laplateforme.io>
2021-09-30 11:17:33 +0200

    job 01_bis finished

--- jour03/job01_bis/main.py
+++ jour03/job01_bis/main.py
@@ -5,24 +5,10 @@ import re
 filename = 'domains.xml'
 
 if exists(filename):
-    tree = ET.parse(filename)
-    root = tree.getroot()
-    print(root)
-    print(root.attrib)
-
-    for x in root[0]:
-        print(x.text)
-
-    #for elem in root:
-    #    for subelem in elem:
-    #        print(subelem.text)
-
-#    count = re.findall(".", )
-#    print("There is ", count.len, "extensions.")
+    file = open(filename)
+    line = file.read()
+    count = re.findall("domain", line)
+    print("There is ", len(count), "extensions.")
 
 else:
     print("File does not exist.")
-
-# Créer un programme qui parcourt le contenu du fichier “domains.xml” et qui
-# compte le nombre d’extension de domain qui s’y trouvent (.com, .net, etc
-# ...).
--- jour03/job02/main.py
+++ jour03/job02/main.py
@@ -7,8 +7,9 @@ if exists(filename):
     file = open(filename)
     text = file.read()
     count = text.split()
+    for word in count:
+        if word == ''
     print(len(count))
     file.close()
 else:
     print("File does not exist.")
-
