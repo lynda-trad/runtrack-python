2c6f9a5aeb604011b00dd729b2f33b91067e0871
lynda-trad <lynda.trad@laplateforme.io>
2021-10-01 16:10:19 +0200

    job08 : WIP

---
+++ jour04/job08/main.py
@@ -0,0 +1,34 @@
+import numpy as np
+
+n = int(input("Entrez un nombre pour la taille : n x n\n"))
+plateau = np.tile('O', (n, n))
+print(plateau)
+print("\n")
+plateau[0][1] = 'X'
+print(plateau)
+ini = 0
+
+
+def game(n, init):
+    if init == 0:
+        x = 0
+        y = 0
+        init = 1
+    if n % 2 == 0 or (n % 3 == 0 and n >= 3):
+        while y != n:
+            if x + 2 <= n and y + 1 <= n:
+                plateau[x + 2][y + 1] = 'X'
+                x += 1
+                y += 1
+            elif x + 2 > n >= y + 1:
+                y += 1
+                x = 0
+            else:
+                return True
+    else:
+        print("Impossible de placer les dames sur un plateau", n, "x", n)
+        return False
+
+
+print("Placement des pieces possible : ", game(n, ini))
+print(plateau)
---
+++ jour04/job24/main.py
@@ -0,0 +1,11 @@
+# 1 si identiques
+# 0 si pas identiques
+def compare(ch1, ch2):
+    return False
+
+chaine1 = input("Entrez la chaine de caracteres 1")
+chaine2 = input("Entrez la chaine de caracteres 2")
+
+chaine1.lower()
+chaine2.lower()
+print(compare(chaine1, chaine2))
